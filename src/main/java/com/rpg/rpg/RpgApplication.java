41b7518c98bf59dd313ec30bfdb2f6dd58f4bb5e
t543115 <joheinen-ext@airfrance.fr>
2018-07-20 10:37:57 +0200

    Swagger

--- src/main/java/com/rpg/rpg/RpgApplication.java
+++ src/main/java/com/rpg/rpg/RpgApplication.java
@@ -7,7 +7,14 @@ import java.net.URISyntaxException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.event.EventListener;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 public class RpgApplication {
@@ -39,4 +46,18 @@ public class RpgApplication {
                 }
             }
         }
+        
+        @Configuration
+        @EnableSwagger2
+        public class SwaggerConfig {                                    
+            @Bean
+            public Docket api() { 
+                return new Docket(DocumentationType.SWAGGER_2)  
+                  .select()                                  
+                  .apis(RequestHandlerSelectors.any())              
+                  .paths(PathSelectors.any())                          
+                  .build();                                           
+            }
+        }
+        
 }
